2600cc652c6a348dac7ddbe2b0bda9d0b6ed00d0
vanqui358 <vanqui358@gmail.com>
2022-10-18 20:43:31 +0700

    content

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -13,17 +13,79 @@
 
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article  <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<div class="main-content" >
+	<div class="main-content-date col-md-4 col-xs-3">
+<h2 class="content-date-day" ><?php echo 
+get_the_date('d',$post_id); ?>
+</h2>
+<p class="content-month">
+THÁNG
+<?php echo
+get_the_date('m',$post_id); ?>
+</p>
 
-	<?php
+</div>
+<div class="main-content-text col-md-8 col-xs-9">
+<header class="entry-header <?php echo esc_attr( $entry_header_classes ); ?>">
 
-	get_template_part( 'template-parts/entry-header' );
+	<div class="entry-header-inner section-inner medium">
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
-	}
+		<?php
+		/**
+		 * Allow child themes and plugins to filter the display of the categories in the entry header.
+		 *
+		 * @since Twenty Twenty 1.0
+		 *
+		 * @param bool Whether to show the categories in header. Default true.
+		 */
+		$show_categories = apply_filters( 'twentytwenty_show_categories_in_entry_header', true );
 
-	?>
+		if ( true === $show_categories && has_category() ) {
+			?>
+
+			<!-- <div class="entry-categories">
+				<span class="screen-reader-text"><?php //_e( 'Categories', 'twentytwenty' ); ?></span>
+				<div class="entry-categories-inner">
+					<?php //the_category( ' ' ); ?>
+				</div>
+			</div> -->
+
+			<?php
+		}
+
+		if ( is_singular() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} else {
+			the_title( '<h2 class="entry-title heading-size-1"><a class="content-title" style="color: #428bca;" href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+		}
+
+		$intro_text_width = '';
+
+		if ( is_singular() ) {
+			$intro_text_width = ' small';
+		} else {
+			$intro_text_width = ' thin';
+		}
+
+		if ( has_excerpt() && is_singular() ) {
+			?>
+
+			<div class="intro-text section-inner max-percentage<?php echo $intro_text_width; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
+				<?php the_excerpt(); ?>
+			</div>
+
+			<?php
+		}
+
+		// Default to displaying the post meta.
+		twentytwenty_the_post_meta( get_the_ID(), 'single-top' );
+		?>
+
+	</div><!-- .entry-header-inner -->
+
+</header><!-- .entry-header -->
+
 
 	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
 	
@@ -68,7 +130,7 @@
 		?>
 
 	</div><!-- .section-inner -->
-
+	</div>
 	<?php
 
 	if ( is_single() ) {
@@ -93,5 +155,5 @@
 		<?php
 	}
 	?>
-
+</div>
 </article><!-- .post -->
